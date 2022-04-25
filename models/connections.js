ea983dcada19a67d241a6040b29efd365fc5a7c6
Deanna K <keysdeanna@gmail.com>
2022-04-25 12:40:00 -0400

    sets us connection.js

---
+++ models/connections.js
@@ -0,0 +1,22 @@
+//////////////////////////
+// Importing Dependencies
+//////////////////////////
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+//////////////////////////////
+// Setup Database Connection
+//////////////////////////////
+const DATABASE_URL = process.env.DATABASE_URL
+mongoose.connect(DATABASE_URL)
+const cxn = mongoose.connection
+
+cxn
+.on('open', () => console.log('Mongo is Open'))
+.on('closed', () => console.log('Mongo is closed'))
+.on('error', (err) => console.log(err))
+
+//////////////////////
+//Export the connection
+/////////////////////////
+module.exports = mongoose
