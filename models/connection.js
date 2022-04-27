1458d2fa07052c95ce08969fd5924acb4e72cb37
Deanna K <keysdeanna@gmail.com>
2022-04-27 07:02:45 -0400

    takes away part of connection object

--- models/connection.js
+++ models/connection.js
@@ -9,8 +9,6 @@ const mongoose = require('mongoose')
 //////////////////////////////
 const DATABASE_URL = process.env.DATABASE_URL
 mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
 })
