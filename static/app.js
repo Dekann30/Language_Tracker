645eb0ee07f5c5cef962f588e7e3626dc5847b08
Deanna K <keysdeanna@gmail.com>
2022-04-27 14:53:02 -0400

    adds minimal styling

--- static/app.js
+++ static/app.js
@@ -1,13 +1,13 @@
 console.log('JS connected')
 
-async function getData() {
-    const response = await fetch('https://dk-languagetracker.herokuapp.com/api')
-    const json = await response.json()
-}
+// async function getData() {
+//     const response = await fetch('https://dk-languagetracker.herokuapp.com/api')
+//     const json = await response.json()
+// }
 
-getData()
+// getData()
 
-$.ajax(json).then((data)=> {
-    const bgPic = data.urls.raw
-    $('body').css('background-image', `url('${bgPic}')`)
-})
+// $.ajax(json).then((data)=> {
+//     const bgPic = data.urls.raw
+//     $('body').css('background-image', `url('${bgPic}')`)
+// })
