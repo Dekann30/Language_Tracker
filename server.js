f817031af4e3c71c4ee6b6fd41e4f83e8941429b
Deanna K <keysdeanna@gmail.com>
2022-04-27 13:59:49 -0400

    attempts to add background pictures

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const methodOverride = require('method-override')
 const morgan = require('morgan')
 const VocabRouter = require('./controllers/VocabController')
 const ConceptRouter = require('./controllers/ConceptController')
+const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -20,9 +22,16 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('static'))
 app.use('/concepts', ConceptRouter)
 app.use('/vocab', VocabRouter)
+app.use(cors())
 
 app.get('/', (req,res)=>{
     res.redirect('/concepts')
+})
+
+app.get('/api', async (req,res)=>{
+    await axios(`https://api.unsplash.com/photos/random?client_id=${APIKEY}&query=japan`).then(data=>{
+        res.json(data.data)
+    })
 })
 //////////////////
 //Server Listener
--- static/app.js
+++ static/app.js
@@ -1,1 +1,13 @@
 console.log('JS connected')
+
+async function getData() {
+    const response = await fetch('https://dk-languagetracker.herokuapp.com/api')
+    const json = await response.json()
+}
+
+getData()
+
+$.ajax(json).then((data)=> {
+    const bgPic = data.urls.raw
+    $('body').css('background-image', `url('${bgPic}')`)
+})
